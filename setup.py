d8da10df4798d64bbd16f35d3cb3cbd6f0fe8c40
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2023-09-11 14:18:53 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,6 @@
 #!/bin/env python
 
-import os
-from setuptools import setup, find_packages
-from glob import glob
-
-def packagez(base):
-    return  ["{0!s}{1!s}{2!s}".format(base, os.path.sep, p) for p in find_packages(base)]
+from setuptools import setup
 
 setup(
     name = 'pyccp',
@@ -15,19 +10,8 @@ setup(
     author = 'Christoph Schueler',
     author_email = 'cpu12.gems@googlemail.com',
     url = 'http://github.com/pySART/pyccp',
-    packages = packagez('pyccp'),
+    packages = ['pyccp'],
     install_requires = ['enum34', 'future', 'mako'],    # 'mock'
-    #entry_points = {
-    #    'console_scripts': [
-    #            'readelf.py = objutils.tools.readelf:main',
-    #    ],
-    #},
-    #data_files = [
-    #    ('objutils/tests/ELFFiles', glob('objutils/tests/ELFFiles*.*')),
-    #],
-    package_dir = {'tests': 'pyccp/tests'},
-    #package_data = {'tests': ['ELFFiles/*.*']},
-    #include_package_data = True,
     test_suite = "pyccp.tests"
 )
 
