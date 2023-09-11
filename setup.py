406487b2461c1d67d34c18230012271da1b24f00
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2023-09-11 14:59:41 +0600

    misc: Clean up

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 #!/bin/env python
 
+import os
 from setuptools import setup
 
-setup(
-    name = 'pyccp',
-    version = '0.9.0',
-    provides = ['pyccp'],
-    description = "CAN Calibration Protocol for Python",
-    author = 'Christoph Schueler',
-    author_email = 'cpu12.gems@googlemail.com',
-    url = 'http://github.com/pySART/pyccp',
-    packages = ['pyccp'],
-    install_requires = [],    # 'mock'
-    test_suite = "pyccp.tests"
-)
 
+setup(
+    name='pyccp',
+    version='0.9.0',
+    description="CAN Calibration Protocol for Python",
+    author='Christoph Schueler',
+    author_email='cpu12.gems@googlemail.com',
+    url='http://github.com/pySART/pyccp',
+    packages=['pyccp'],
+    test_suite="pyccp.tests"
+)
