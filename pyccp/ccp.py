fbcb1421814915a6bb54a33b8991c7ba8a6e5164
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2025-10-10 14:33:55 +0200

    ccp: Align code style and add copyright update.

--- pyccp/ccp.py
+++ pyccp/ccp.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__copyright__="""
+__copyright__ = """
     pySART - Simplified AUTOSAR-Toolkit for Python.
 
    (C) 2009-2016 by Christoph Schueler <cpu12.gems@googlemail.com>
+   (C) 2023-2025 by Vladimir Petrigo <vladimir.petrigo@gmail.com>
 
    All Rights Reserved
 
@@ -23,75 +24,74 @@ __copyright__="""
   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
 
-from collections import namedtuple
 import enum
-from pprint import pprint
 import struct
-
+from collections import namedtuple
+from pprint import pprint
 
 MAX_CTO = 0x0008
 MAX_DTO = 0x0008
 
 CCP_VERSION = (2, 1)
 
-class CommandCodes(enum.IntEnum):
 
+class CommandCodes(enum.IntEnum):
     # Mandatory Commands.
-    CONNECT             = 0x01
-    GET_CCP_VERSION     = 0x1B
-    EXCHANGE_ID         = 0x17
-    SET_MTA             = 0x02
-    DNLOAD              = 0x03
-    UPLOAD              = 0x04
-    GET_DAQ_SIZE        = 0x14
-    SET_DAQ_PTR         = 0x15
-    WRITE_DAQ           = 0x16
-    START_STOP          = 0x06
-    DISCONNECT          = 0x07
+    CONNECT = 0x01
+    GET_CCP_VERSION = 0x1B
+    EXCHANGE_ID = 0x17
+    SET_MTA = 0x02
+    DNLOAD = 0x03
+    UPLOAD = 0x04
+    GET_DAQ_SIZE = 0x14
+    SET_DAQ_PTR = 0x15
+    WRITE_DAQ = 0x16
+    START_STOP = 0x06
+    DISCONNECT = 0x07
     # Optional Commands.
-    GET_SEED            = 0x12
-    UNLOCK              = 0x13
-    DNLOAD_6            = 0x23
-    SHORT_UP            = 0x0F
-    SELECT_CAL_PAGE     = 0x11
-    SET_S_STATUS        = 0x0C
-    GET_S_STATUS        = 0x0D
-    BUILD_CHKSUM        = 0x0E
-    CLEAR_MEMORY        = 0x10
-    PROGRAM             = 0x18
-    PROGRAM_6           = 0x22
-    MOVE                = 0x19
-    TEST                = 0x05
+    GET_SEED = 0x12
+    UNLOCK = 0x13
+    DNLOAD_6 = 0x23
+    SHORT_UP = 0x0F
+    SELECT_CAL_PAGE = 0x11
+    SET_S_STATUS = 0x0C
+    GET_S_STATUS = 0x0D
+    BUILD_CHKSUM = 0x0E
+    CLEAR_MEMORY = 0x10
+    PROGRAM = 0x18
+    PROGRAM_6 = 0x22
+    MOVE = 0x19
+    TEST = 0x05
     GET_ACTIVE_CAL_PAGE = 0x09
-    START_STOP_ALL      = 0x08
-    DIAG_SERVICE        = 0x20
-    ACTION_SERVICE      = 0x21
+    START_STOP_ALL = 0x08
+    DIAG_SERVICE = 0x20
+    ACTION_SERVICE = 0x21
 
 
 class ReturnCodes(enum.IntEnum):
-    ACKNOWLEDGE                     = 0x00
-    DAQ_PROCESSOR_OVERLOAD          = 0x01    # C0
-    COMMAND_PROCESSOR_BUSY          = 0x10    # C1 NONE (wait until ACK or timeout)
-    DAQ_PROCESSOR_BUSY              = 0x11    # C1 NONE (wait until ACK or timeout)
-    INTERNAL_TIMEOUT                = 0x12    # C1 NONE (wait until ACK or timeout)
-    KEY_REQUEST                     = 0x18    # C1 NONE (embedded seed&key)
-    SESSION_STATUS_REQUEST          = 0x19    # C1 NONE (embedded SET_S_STATUS)
-    COLD_START_REQUEST              = 0x20    # C2 COLD START
-    CAL_DATA_INIT_REQUEST           = 0x21    # C2 cal. data initialization
-    DAQ_LIST_INIT_REQUEST           = 0x22    # C2 DAQ list initialization
-    CODE_UPDATE_REQUEST             = 0x23    # C2 (COLD START)
-    UNKNOWN_COMMAND                 = 0x30    # C3 (FAULT)
-    COMMAND_SYNTAX                  = 0x31    # C3 FAULT
-    PARAMETER_OUT_OF_RANGE          = 0x32    # C3 FAULT
-    ACCESS_DENIED                   = 0x33    # C3 FAULT
-    OVERLOAD                        = 0x34    # C3 FAULT
-    ACCESS_LOCKED                   = 0x35    # C3 FAULT
-    RESOURCE_FUNCTION_NOT_AVAILABLE = 0x36    # C3 FAULT
+    ACKNOWLEDGE = 0x00
+    DAQ_PROCESSOR_OVERLOAD = 0x01  # C0
+    COMMAND_PROCESSOR_BUSY = 0x10  # C1 NONE (wait until ACK or timeout)
+    DAQ_PROCESSOR_BUSY = 0x11  # C1 NONE (wait until ACK or timeout)
+    INTERNAL_TIMEOUT = 0x12  # C1 NONE (wait until ACK or timeout)
+    KEY_REQUEST = 0x18  # C1 NONE (embedded seed&key)
+    SESSION_STATUS_REQUEST = 0x19  # C1 NONE (embedded SET_S_STATUS)
+    COLD_START_REQUEST = 0x20  # C2 COLD START
+    CAL_DATA_INIT_REQUEST = 0x21  # C2 cal. data initialization
+    DAQ_LIST_INIT_REQUEST = 0x22  # C2 DAQ list initialization
+    CODE_UPDATE_REQUEST = 0x23  # C2 (COLD START)
+    UNKNOWN_COMMAND = 0x30  # C3 (FAULT)
+    COMMAND_SYNTAX = 0x31  # C3 FAULT
+    PARAMETER_OUT_OF_RANGE = 0x32  # C3 FAULT
+    ACCESS_DENIED = 0x33  # C3 FAULT
+    OVERLOAD = 0x34  # C3 FAULT
+    ACCESS_LOCKED = 0x35  # C3 FAULT
+    RESOURCE_FUNCTION_NOT_AVAILABLE = 0x36  # C3 FAULT
 
 
 class DTOType(enum.IntEnum):
-    COMMAND_RETURN_MESSAGE  = 255
-    EVENT_MESSAGE           = 254
+    COMMAND_RETURN_MESSAGE = 255
+    EVENT_MESSAGE = 254
 
 
 class State(enum.IntEnum):
@@ -99,51 +99,43 @@ class State(enum.IntEnum):
 
 
 class CRO(object):
-    """Command Receive Object.
-    """
+    """Command Receive Object."""
 
 
 class DTO(object):
-    """Data Transmission Object.
-    """
+    """Data Transmission Object."""
 
-    COMMAND_RETURN_MESSAGE  = 255
-    EVENT_MESSAGE           = 254
+    COMMAND_RETURN_MESSAGE = 255
+    EVENT_MESSAGE = 254
 
-    def send(self, pid, err, ctr, b0 = 0, b1 = 0, b2 = 0, b3 = 0, b4 = 0):
-        """Transfer up to 5 data bytes from slave (ECU) to master.
-        """
+    def send(self, pid, err, ctr, b0=0, b1=0, b2=0, b3=0, b4=0):
+        """Transfer up to 5 data bytes from slave (ECU) to master."""
         pass
 
 
 class CRM(DTO):
-    """Command Return Message.
-    """
+    """Command Return Message."""
 
 
 class EVM(DTO):
-    """Event Message.
-    """
+    """Event Message."""
 
 
 class DAQ(object):
-    """Data Acquisition Message.
-    """
+    """Data Acquisition Message."""
 
-    def send(self, pid, b0 = 0, b1 = 0, b2 = 0, b3 = 0, b4 = 0, b5 = 0, b6 = 0):
-        """Transfer up to 7 data bytes from slave (ECU) to master.
-        """
+    def send(self, pid, b0=0, b1=0, b2=0, b3=0, b4=0, b5=0, b6=0):
+        """Transfer up to 7 data bytes from slave (ECU) to master."""
         pass
 
 
 class ODT(object):
-    """Object Descriptor Table.
-    """
+    """Object Descriptor Table."""
 
 
 class DAQList(object):
-    """Data Acquisition List.
-    """
+    """Data Acquisition List."""
+
     """
     2401
     360a
@@ -152,11 +144,12 @@ class DAQList(object):
     360d
     """
 
+
 # File format not recognized.
 
-class CANMessageObject(object):
 
-    def __init__(self, canID, dlc, data, extendedAddr = False, rtr = False):
+class CANMessageObject(object):
+    def __init__(self, canID, dlc, data, extendedAddr=False, rtr=False):
         self.canID = canID
         self.dlc = dlc
         self.data = data
@@ -164,7 +157,7 @@ class CANMessageObject(object):
         self.rtr = rtr
 
     def __str__(self):
-        addrFmt =  "[{:08X}]  " if self.extendedAddr else "{:04X}  "
+        addrFmt = "[{:08X}]  " if self.extendedAddr else "{:04X}  "
         fmt = addrFmt + "{}"
         return fmt.format(self.canID, " ".join(["{:02X}".format(x) for x in self.data]))
 
@@ -172,16 +165,19 @@ class CANMessageObject(object):
 
 
 class MockTransport(object):
-
     def __init__(self):
         self.parent = None
 
-    def send(self, canID, b0 = 0, b1 = 0, b2 = 0, b3 = 0, b4 = 0, b5 = 0, b6 = 0, b7 = 0):
-        self.message = CANMessageObject(canID, 8, bytearray((b0, b1, b2, b3, b4, b5, b6, b7)))
-        #print("Sending: {}".format(self.message))
+    def send(self, canID, b0=0, b1=0, b2=0, b3=0, b4=0, b5=0, b6=0, b7=0):
+        self.message = CANMessageObject(
+            canID, 8, bytearray((b0, b1, b2, b3, b4, b5, b6, b7))
+        )
+        # print("Sending: {}".format(self.message))
 
-    def receive(self, canID, b0 = 0, b1 = 0, b2 = 0, b3 = 0, b4 = 0, b5 = 0, b6 = 0, b7 = 0):
-        self.message = CANMessageObject(canID, 8, bytearray((b0, b1, b2, b3, b4, b5, b6, b7)))
+    def receive(self, canID, b0=0, b1=0, b2=0, b3=0, b4=0, b5=0, b6=0, b7=0):
+        self.message = CANMessageObject(
+            canID, 8, bytearray((b0, b1, b2, b3, b4, b5, b6, b7))
+        )
         self.parent.receive(self.message)
 
     def __str__(self):
@@ -191,7 +187,5 @@ class MockTransport(object):
 
 
 class Memory(object):
-
     def __init__(self):
         pass
-
